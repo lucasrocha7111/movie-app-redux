115d045c3da514017a066274d489e6fc961ceaac
Lucas Rocha <lucasrocha7111@gmail.com>
2020-05-22 00:03:14 -0300

    Add device language support

--- src/utils/videoutil.js
+++ src/utils/videoutil.js
@@ -1,8 +1,13 @@
+import I18n from 'react-native-i18n'
 
 export class VideoUtil {
 
     static getFieldByType(type) {
         return type === 'movie' ? 'title' : 'name'
+    }
+
+    static getLanguage() {
+        return I18n.currentLocale()
     }
     
 }
